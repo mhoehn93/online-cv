4473c6971103aadca18a7b48dc288f5120d43482
mhoehn93 <manuel.hoehn@students.ffhs.ch>
2025-03-10 22:32:51 +0100

    Many changes

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,23 @@ function toggleMenu() {
         setTimeout(() => {nav.style.display = 'none'}, 400);
     }
 }
+
+function scrollToAnchor(target) {
+    const targetElement = document.getElementById(`${target}`);
+    const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - 52;
+    console.log(targetPosition);
+    window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+    });
+    toggleMenu();
+}
+
+window.onload = function() {
+    const elements = document.querySelectorAll("body");
+    elements.forEach((element, index) => {
+        setTimeout(() => {
+            element.classList.add("fade-in");
+        }, index * 100); // Delay each element by 100ms
+    });
+}
